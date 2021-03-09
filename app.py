73df99c2c0ef56b6d3920fcff34fa100c5a38485
Ethan <ethanlesch8@gmail.com>
2021-03-09 16:06:33 +0200

    update

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def dict_factory(cursor, row):
 
 @app.route('/food/', methods=['GET'])
 def show_data():
-    with sqlite3.connect('calories.db') as con:
+    with sqlite3.connect('calories') as con:
         con.row_factory = dict_factory
         cursor = con.cursor()
-        cursor.execute("SELECT * FROM 'calorie track'")
+        cursor.execute("SELECT * FROM 'calorie_track'")
         data = cursor.fetchall()
     return jsonify(data)
 
@@ -29,7 +29,7 @@ def search_Food():
         post_data = request.get_json()
         morning = post_data['morning']
         afternoon = post_data['afternoon']
-        # evening = post_data['evening']
+        evening = post_data['evening']
 
         with sqlite3.connect('calories') as con:
             con.row_factory = dict_factory
@@ -42,6 +42,13 @@ def search_Food():
             cur = con.cursor()
             cur.execute("SELECT * FROM calorie_track WHERE foods=?", (afternoon,))
             data.append(cur.fetchall())
+
+        with sqlite3.connect('calories') as con:
+            con.row_factory = dict_factory
+            cur = con.cursor()
+            cur.execute("SELECT * FROM calorie_track WHERE foods=?", (evening,))
+            data.append(cur.fetchall())
+
     except Exception as e:
         print('Something went wrong while feaching calorie track: ' + str(e))
     finally:
